8acad329b5e41c6411447ab623f60a1f9ebd2025
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-07 17:29:39 +0530

    add item initial UI commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import AddItem from './components/AddItems';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import ItemsList from './components/ItemList';
 
 function App() {
   return (
+    <div>
+       <Header/>
+    
     <div className="continer">
-      <Header/>
+     
       <AddItem/>
       <ItemsList/>
       <Footer/>
       
+    </div>
     </div>
   );
 }
--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -1,5 +1,28 @@
 export default function AddItem(){
     return(
-        <div>Add items</div>
+        <div>
+            <form  className="add-item">
+                <div>
+                    <label htmlFor="">Item Name</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label htmlFor="">Quantity</label>
+                    <select >
+                        <option >1</option>
+                        <option >2</option>
+                        <option >3</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="">Price</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <button className="btn">Add Item</button>
+                </div>
+            </form>
+
+        </div>
     )
 }
